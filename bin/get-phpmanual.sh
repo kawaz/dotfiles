0f8ee8e5cd45277140eded42cf7d188c35d2588c
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-01-20 16:01:06 +0900

    ファイルダウンロードにcurlを使うようにした

--- bin/get-phpmanual.sh
+++ bin/get-phpmanual.sh
@@ -2,4 +2,5 @@
 cd "`dirname "$0"`"/../skel || exit 1
 mkdir -p .vim/phpmanual-cache || exit 1
 cd       .vim/phpmanual-cache || exit 1
-wget -O- http://jp.php.net/get/php_manual_ja.tar.gz/from/this/mirror | tar xfz -
+echo Download phpmanual for ref.vim
+curl -L --progress-bar http://jp.php.net/get/php_manual_ja.tar.gz/from/this/mirror | tar xfz -
