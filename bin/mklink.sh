6a1d6f5a5a4508cf8f211f87cdaec4f57af66bb5
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-01-20 16:02:27 +0900

    既存の設定ファイルがあった場合はバックアップディレクトリに移動して上書きしないようにした

--- bin/mklink.sh
+++ bin/mklink.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 cd "`dirname "$0"`"/../skel || exit 1
+backupdir="$HOME/dotfiles-backup-`date +%Y%m%dT%H%M%S`"
 
 find "`pwd`" -mindepth 1 -maxdepth 1 -name .\* ! -name .\*.swp |
 while read f; do
-  ln -sfn "$f" ~/"`basename "$f"`"
+  link="$HOME/${f##*/}"
+  # 既存の実ファイルが存在したらリネームしてとっておく(symlinkは上書きするので放置)
+  if [ -e "$link" -a ! -L "$link" ]; then
+    mkdir -p "$backupdir"
+    mv "$link" "$backupdir/${f##*/}"
+  fi
+  ln -sfn "$f" "$link"
 done
+
+if [ -d "$backupdir" ]; then
+  echo -e "既存のドットファイルは \x1b[36m${backupdir}\x1b[0m に移動されました"
+fi
