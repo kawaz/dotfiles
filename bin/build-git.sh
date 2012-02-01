0d633d57c5230be1513f806354ac18b3273df36d
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-02-01 14:45:37 +0900

    最新のgitを使えるようにするスクリプトを作成

---
+++ bin/build-git.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+bin="`dirname "$0"`"
+env="$bin/../env"
+
+mkdir -p "$env" && cd "$env" || exit 1
+env="`pwd`"
+src="$env/src/git"
+dest="$env/dest/git"
+mkdir -p "$src" "$dest" || exit 1
+
+if [ ! -d "$src" ]; then
+  git clone git://github.com/gitster/git.git "$src" || exit 1
+fi
+cd "$src" || exit 1
+git pull
+sudo yum install -y curl-devel expat-devel perl-ExtUtils-MakeMaker
+make configure && ./configure --prefix="$dest" && make && make install || exit 1
+
+mkdir -p ~/.profile.d || exit 1
+echo "export PATH=\"$dest/bin:\$PATH\"" > ~/.profile.d/dotfiles-git.sh
