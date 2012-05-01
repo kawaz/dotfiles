1529408ff8464333da13b79481ad53f91ecd0a72
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-05-01 16:20:18 +0900

    keychainの出力抑制の記述ミスを修正

--- skel/.profile.d/20-keychain.sh
+++ skel/.profile.d/20-keychain.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 which --skip-alias --skip-function keychain >/dev/null 2>&1
 if [ "$?" == "0" ]; then
-  keychain -q --agents ssh 2>/dev/null
+  keychain -q --agents ssh >/dev/null
   if [ -f ~/.keychain/$HOSTNAME-sh ]; then
     . ~/.keychain/$HOSTNAME-sh
   fi
