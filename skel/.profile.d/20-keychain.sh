504d7767cf1e730b284fbbbfa4c694b6c315b9f5
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-05-01 15:57:34 +0900

    エラー出力のハンドリングミスを修正

--- skel/.profile.d/20-keychain.sh
+++ skel/.profile.d/20-keychain.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-which --skip-alias --skip-function keychain >/dev/null >&1
+which --skip-alias --skip-function keychain >/dev/null 2>&1
 if [ "$?" == "0" ]; then
   keychain -q --agents ssh 2>/dev/null
   if [ -f ~/.keychain/$HOSTNAME-sh ]; then
