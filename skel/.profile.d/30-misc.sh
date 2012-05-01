cb14168feab685a4b945554eb6be9363ca0301f7
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-05-01 13:03:55 +0900

    環境に合わせてベターなTERMを使えるようにした

--- skel/.profile.d/30-misc.sh
+++ skel/.profile.d/30-misc.sh
@@ -3,7 +3,12 @@ export LANG="ja_JP.UTF-8"
 export PAGER="less -R"
 export EDITOR="vim"
 export PS1='`[[ "$?" -eq 0 ]]&&echo "\[\e[1;34m\]"||echo "\[\e[1;31m\]"`[`date +%d%a%T` \u@\h \W]\$\[\e[m\] '
-export TERM="screen-256color"
+for TERM in screen-256color screen xterm vt100; do
+  if [ -f /usr/*/terminfo/*/"$TERM" ]; then
+    export TERM
+    break;
+  fi
+done
 
 alias du0='du --max-depth=1'
 alias du1='du --max-depth=1'
