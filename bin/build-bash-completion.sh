e8e790c1e1ae446119f776ced8e63078faffaabe
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-06-26 15:30:50 +0900

    bash-completionを古い環境用に入れるスクリプト

---
+++ bin/build-bash-completion.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+bin="`dirname "$0"`"
+env="$bin/../env"
+
+mkdir -p "$env" && cd "$env" || exit 1
+env="`pwd`"
+dest="$env/dest/bash-completion"
+
+
+bash=${BASH_VERSION%.*}; bmajor=${bash%.*}; bminor=${bash#*.}
+if [ $bmajor -lt 4 ] || [ $bmajor -eq 4 -a $bminor -lt 1 ]; then
+  # bash < 4.1
+  mkdir -p "$env/src"
+  cd "$env/src" || exit 1
+  curl http://bash-completion.alioth.debian.org/files/bash-completion-1.3.tar.bz2 | tar xfj -
+  cd bash-completion-1.3 || exit 1
+  ./configure --prefix="$dest" && make && make install
+  ( echo '#!/bin/sh'
+    echo "BASH_COMPLETION=\"$dest/etc/bash_completion\""
+    echo "BASH_COMPLETION_DIR=\"$dest/etc/bash_completion.d\""
+    echo "BASH_COMPLETION_COMPAT_DIR=\"$dest/etc/bash_completion.d\""
+    echo ". \"$dest/etc/bash_completion\""
+  ) > ~/.profile.d/10-dotfiles-bash-completion.sh
+fi
