e856c592e38a005b598713f32fc1334323fed515
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-10-16 13:23:23 +0900

    tmux-1.7がビルドできるようにした

--- bin/build-tmux.sh
+++ bin/build-tmux.sh
@@ -4,19 +4,31 @@ env="$base/env"
 
 mkdir -p "$env" && cd "$env" || exit 1
 env="`pwd`"
-src="$env/src/tmux-1.6"
+src="$env/src/tmux"
 dest="$env/dest/tmux"
 
-if [ ! -d "$src/.svn" ]; then
-  svn co https://tmux.svn.sourceforge.net/svnroot/tmux/tags/TMUX_1_6 "$src" || exit 1
-fi
+# develop version
+rm -rf "$src"
+git clone git://tmux.git.sourceforge.net/gitroot/tmux/tmux "$src"
 cd "$src" || exit 1
-svn up || exit 1
 
-# 罫線パッチ http://d.hatena.ne.jp/emonkak/20110521/1305970697
+# 参考：罫線パッチ http://d.hatena.ne.jp/emonkak/20110521/1305970697
 patch -p1 < "$base/bin/build-tmux.sh.border_patch"
 
 sudo yum install -y automake libevent-devel ncurses-devel || exit 1
+
+#libevent2が必要なので1.xしか無い場合はソースで入れる
+if rpm -q libevent-devel-1.\* >/dev/null 2>&1; then
+  ( src="$env/src/libevent2"; mkdir -p "$src" && cd "$src" || exit 1
+    curl -L https://github.com/downloads/libevent/libevent/libevent-2.0.20-stable.tar.gz | tar xz
+    cd libevent-2.0.20-stable || exit 1
+    ./configure --prefix="$env/dest/libevent2" && make && make install
+  ) || exit 1
+  export CPPFLAGS="-I$env/dest/libevent2/include"
+  export LDFLAGS="-L$env/dest/libevent2/lib"
+  echo "export LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:$env/dest/libevent2/lib\"" > ~/.profile.d/10-dotfiles-tmux-libevent2.sh
+fi
+
 sh autogen.sh || exit 1
 ./configure --prefix="$dest" && make && make install || exit 1
 
