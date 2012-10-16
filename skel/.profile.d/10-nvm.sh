59a784cdf30757f3d927d931ad5bbe1ddf54fdc8
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-10-16 11:46:56 +0900

    npmが無い場合はnpm補完をしないようにした

--- skel/.profile.d/10-nvm.sh
+++ skel/.profile.d/10-nvm.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 if [ -s ~/.nvm/nvm.sh ]; then
   . ~/.nvm/nvm.sh
-  . <(npm completion)
+  if which --skip-alias --skip-functions npm >/dev/null 2>&1; then
+    . <(npm completion)
+  fi
 fi
