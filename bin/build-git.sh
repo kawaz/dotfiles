d7491e903c7516df7e04b381a50653adb4faad77
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-10-24 05:22:59 +0900

    ディレクトリ構成をリファクタリング

--- bin/build-git.sh
+++ bin/build-git.sh
@@ -6,19 +6,19 @@ mkdir -p "$env" && cd "$env" || exit 1
 env="`pwd`"
 src="$env/src/git"
 dest="$env/dest/git"
-mkdir -p "$src" "$dest" || exit 1
+prodile_d="$env/profile.d"
+mkdir -p "$src" "$dest" "$prodile_d" || exit 1
 
 if [ ! -d "$src/.git" ]; then
   git clone git://github.com/gitster/git.git "$src" || exit 1
 fi
 cd "$src" || exit 1
-git pull
+git pull --rebase
 sudo yum install -y curl-devel expat-devel perl-ExtUtils-MakeMaker gettext autoconf
 make configure && ./configure --prefix="$dest" && make && make install || exit 1
 
-mkdir -p ~/.profile.d || exit 1
 ( echo "export PATH=\"$dest/bin:\$PATH\""
   echo "if [ -r \"$src/contrib/completion/git-completion.bash\" ]; then "
   echo "  . \"$src/contrib/completion/git-completion.bash\""
   echo "fi"
-) > ~/.profile.d/10-dotfiles-git.sh
+) > "$prodile_d"/10-git.sh
