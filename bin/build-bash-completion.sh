261a5aec2696281e4f8a28464034d780139f24b0
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-10-24 04:06:06 +0900

    リファクタリング

--- bin/build-bash-completion.sh
+++ bin/build-bash-completion.sh
@@ -5,11 +5,12 @@ env="$bin/../env"
 mkdir -p "$env" && cd "$env" || exit 1
 env="`pwd`"
 dest="$env/dest/bash-completion"
-
+profile_d="$env/profile.d"
 
 bash=${BASH_VERSION%.*}; bmajor=${bash%.*}; bminor=${bash#*.}
 if [ $bmajor -lt 4 ] || [ $bmajor -eq 4 -a $bminor -lt 1 ]; then
   # bash < 4.1
+  mkdir -p "$profile_d"
   mkdir -p "$env/src"
   cd "$env/src" || exit 1
   curl http://bash-completion.alioth.debian.org/files/bash-completion-1.3.tar.bz2 | tar xfj -
@@ -22,5 +23,5 @@ if [ $bmajor -lt 4 ] || [ $bmajor -eq 4 -a $bminor -lt 1 ]; then
     echo "if [ -r \"$dest/etc/bash_completion\" ]; then"
     echo "  . \"$dest/etc/bash_completion\""
     echo "fi"
-  ) > ~/.profile.d/10-dotfiles-bash-completion.sh
+  ) > "$profile_d"/10-dotfiles-bash-completion.sh
 fi
