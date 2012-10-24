bc883635631f6487d1b8d8ab4a5a6508e50499a4
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-10-24 23:42:58 +0900

    NeoBundleInstallを早くした

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,6 +2,6 @@
 cd "`dirname "$0"`"/.. || exit
 git submodule update --init
 sh ./bin/mklink.sh
-vim -c ':NeoBundleInstall!' -c ':qa!'
+vim -c ':NeoBundleInstall' -c ':qa!'
 sh ./bin/get-phpmanual.sh
 sh ./bin/mkvimdic.sh
