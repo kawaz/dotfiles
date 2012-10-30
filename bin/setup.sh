7f3cae0d9b934cf80cf902986ae1051d4f22b903
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-10-30 17:31:23 +0900

    submodule面倒くなったのでcloneにした

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 cd "`dirname "$0"`"/.. || exit
-git submodule update --init
 
-git submodule add -f https://github.com/Shougo/neobundle.vim env/dot-vim/bundle/neobundle.vim
+git clone https://github.com/Shougo/neobundle.vim env/dot-vim/bundle/neobundle.vim
 ln -sfn ../../env/dot-vim/bundle/neobundle.vim etc/skel/.vim
 sh ./bin/mklink.sh
 vim -c ':NeoBundleInstall' -c ':qa!'
