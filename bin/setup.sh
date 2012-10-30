483a979fca0f7ad46fe895ed6303023ab4bfe5fa
Yoshiaki Kawazu <kawazzz@gmail.com>
2012-10-30 17:16:21 +0900

    .vimの場所を変えた

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd "`dirname "$0"`"/.. || exit
 git submodule update --init
+
+git submodule add -f https://github.com/Shougo/neobundle.vim env/dot-vim/bundle/neobundle.vim
+ln -sfn ../../env/dot-vim/bundle/neobundle.vim etc/skel/.vim
 sh ./bin/mklink.sh
 vim -c ':NeoBundleInstall' -c ':qa!'
 sh ./bin/get-phpmanual.sh
