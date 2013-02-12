84441eac2c62e1283316cabda10513a25ff4b03d
Yoshiaki Kawazu <kawazzz@gmail.com>
2013-02-12 17:51:44 +0900

    npm補完の再定義を少し丁寧にした

--- etc/bash_completion.d/npm.sh
+++ etc/bash_completion.d/npm.sh
@@ -3,6 +3,9 @@
 # 初めてnpmの補完を実行するまで読み込みを遅延する
 function _npm_completion() {
   if which npm >/dev/null 2>&1; then
+    # 補完設定を再定義
+    unset _npm_completion
+    complete -r npm
     . <(npm completion)
   fi
 }
