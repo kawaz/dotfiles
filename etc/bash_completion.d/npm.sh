1e42891d52720c078734522e486b1984ffacef97
Yoshiaki Kawazu <kawazzz@gmail.com>
2013-02-19 16:46:47 +0900

    tiny fix

--- etc/bash_completion.d/npm.sh
+++ etc/bash_completion.d/npm.sh
@@ -2,10 +2,10 @@
 
 # 初めてnpmの補完を実行するまで読み込みを遅延する
 function _npm_completion() {
+  # 補完設定を再定義
+  unset _npm_completion
+  complete -r npm
   if which npm >/dev/null 2>&1; then
-    # 補完設定を再定義
-    unset _npm_completion
-    complete -r npm
     . <(npm completion)
   fi
 }
