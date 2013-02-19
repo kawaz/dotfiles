e1a9b8ab22699cec92de92dbaf2e0a0488d763a0
Yoshiaki Kawazu <kawazzz@gmail.com>
2013-02-19 16:12:11 +0900

    fix comment

--- bin/build-node.sh
+++ bin/build-node.sh
@@ -8,10 +8,9 @@ git clone git://github.com/creationix/nvm.git ~/.nvm
 
 # install stable node
 node_stable=$(curl -s http://nodejs.org/dist/ | egrep -o '[0-9]+\.[0-9]*[02468]\.[0-9]+' | sort -u -k 1,1n -k 2,2n -k 3,3n -t . | tail -n1)
-
 if ls /lib/libc.so* -l | egrep -q 'libc-2\.[0-5]\.'; then
-  has_rpm_packages gcc-c++ || exit 1
   # 最近のnvmはバイナリを落とそうとするがglibcが2.5以下だと動かないので該当する場合はソースビルドさせる
+  has_rpm_packages gcc-c++ || exit 1
   if python -V 2>&1 | grep -q ' 2\.4' && which python26 2>/dev/null; then
     # nodeのビルドスクリプトがpython2.4だと動かないのでpython26があればそれを使わせる
     cd_tmpdir && (ln -s `which python26` python; PATH="`pwd`:$PATH" nvm install -s "v$node_stable")
