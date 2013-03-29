f05dcc916266b428d77a0b7f2d59193feb30875e
Yoshiaki Kawazu <kawazzz@gmail.com>
2013-03-29 17:52:53 +0900

    rvm補完関数の読み込みを遅延させるようにした

--- etc/bash_completion.d/rvm.sh
+++ etc/bash_completion.d/rvm.sh
@@ -1,1 +1,12 @@
-[[ -f ~/.rvm/scripts/completion ]] && . ~/.rvm/scripts/completion
+#!/bin/bash
+
+# 初めてrvmの補完を実行するまで読み込みを遅延する
+function _rvm_completion() {
+  unset _rvm_completion
+  complete -r rvm
+  if [[ -f ~/.rvm/scripts/completion ]]; then
+    . ~/.rvm/scripts/completion && return 124
+  fi
+}
+complete -F _rvm_completion rvm
+
