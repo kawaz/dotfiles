5d55e860181ffccbf0300f3039c59084e06bce3d
Yoshiaki Kawazu <kawazzz@gmail.com>
2014-06-11 17:28:12 +0900

    deprecatedになったnet-toolsの大体コマンドを教える

---
+++ etc/profile.d/deprecated-tools.sh
@@ -0,0 +1,37 @@
+# http://blog.livedoor.jp/sonots/archives/38589335.html
+
+# Macでは無視
+[[ "${OSTYPE:0:6}" == "darwin" ]] && return
+
+net_tools_deprecated_message () {
+  echo 'net-tools コマンドはもう非推奨ですよ？おじさんなんじゃないですか？ '
+}
+
+arp () {
+  net_tools_deprecated_message
+  echo 'Use `ip n`'
+}
+ifconfig () {
+  net_tools_deprecated_message
+  echo 'Use `ip a`, `ip link`, `ip -s link`'
+}
+iptunnel () {
+  net_tools_deprecated_message
+  echo 'Use `ip tunnel`'
+}
+iwconfig () {
+  echo 'iwconfig コマンドはもう非推奨ですよ？おじさんなんじゃないですか？ '
+  echo 'Use `iw`'
+}
+nameif () {
+  net_tools_deprecated_message
+  echo 'Use `ip link`, `ifrename`'
+}
+netstat () {
+  net_tools_deprecated_message
+  echo 'Use `ss`, `ip route` (for netstat -r), `ip -s link` (for netstat -i), `ip maddr` (for netstat -g)'
+}
+route () {
+  net_tools_deprecated_message
+  echo 'Use `ip r`'
+}
