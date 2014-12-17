1d8e609812878b9282b5667a78345b7d4d40b291
Yoshiaki Kawazu <kawazzz@gmail.com>
2014-12-17 16:22:50 +0900

    mac用bash_completion設定を追加

--- etc/profile.d/mac-only.sh
+++ etc/profile.d/mac-only.sh
@@ -1,6 +1,11 @@
 # Mac以外では無視
 [[ "${OSTYPE:0:6}" != "darwin" ]] && return
 
+# bash_completionを読み込む
+if [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
+  . $(brew --prefix)/share/bash-completion/bash_completion
+fi
+
 # DOCKER_HOSTを設定する
 if type boot2docker >/dev/null 2>&1; then
   export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375
