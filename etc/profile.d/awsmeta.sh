9dfd5557bcb5a3f5222b190c9e9f571c0b67d0ed
Yoshiaki Kawazu <kawazzz@gmail.com>
2015-10-20 03:12:06 +0900

    Bugfix: _awsmeta

--- etc/profile.d/awsmeta.sh
+++ etc/profile.d/awsmeta.sh
@@ -23,12 +23,17 @@ if [[ -n "$PS1" ]]; then
     COMPREPLY=()
     local path=$(perl -pe's/[^\/]+?$//'<<<"$cur")
     local basename=${cur##*/}
-    local f
-    while read f; do
-      [[ "$path$f" == $cur* ]] && COMPREPLY+=("$path$f")
+    local f; while read f; do
+      [[ $f == $basename* ]] && COMPREPLY+=("$path$f")
     done < <(awsmeta "$path")
-    # 確定ワードが/終わりの時はスペースを足さない
+    # 確定候補が/終わりなら次の補完を継続できるように空白挿入を抑止する
     [[ ${#COMPREPLY[@]} == 1 && $COMPREPLY =~ /$ ]] && compopt -o nospace
+    # メニュー補完の場合はパス部分を除いたリストで表示する
+    if [[ $COMP_TYPE == 63 ]]; then
+      local i; for ((i = 0; i < ${#COMPREPLY[@]}; i++)); do
+        COMPREPLY[$i]="${COMPREPLY[i]:${#path}}"
+      done
+    fi
   }
   complete -F _awsmeta awsmeta
 fi
