b6935f336fa014caa4f8ea3e88c5e6f418980ae9
Yoshiaki Kawazu <kawazzz@gmail.com>
2015-10-29 18:22:06 +0900

    Refactoring

--- etc/bash_completion.d/_known_hosts_hook.sh
+++ etc/bash_completion.d/_known_hosts_hook.sh
@@ -4,16 +4,33 @@ _known_hosts_real_hook_before() {
   echo; echo "$FUNCNAME"
   set | grep ^COMP
 }
-_known_hosts_real_hook_after() {
-  if [[ $COMP_TYPE == 9 && ${#COMPREPLY[@]} -gt 1 ]]; then
-    local cur=${COMP_WORDS[COMP_CWORD]}
-    if ! _zunlib_equals_all_same_index_char "${#cur}" "${COMPREPLY[@]}"; then
-      local IFS=$'\n'
-      COMPREPLY=($(sort -u <<< "${COMPREPLY[*]}" | peco --query "$cur"))
-      # Call recursive, because peco's result may be multiple.
-      "$FUNCNAME"
-    fi
-  fi
+_known_hosts_real_hook_after()
+{
+  _zunlib_completion_select_with_peco -s -u
+}
+
+# select complete target by peco
+_zunlib_completion_select_with_peco() {
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  # check: peco exists
+  type peco >/dev/null 2>&1 || return 0
+  # check: occasion
+  [[ $COMP_TYPE == 9 && ${#COMPREPLY[@]} -gt 1 ]] || return 0
+  # check: can still auto completion?
+  _zunlib_equals_all_same_index_char "${#cur}" "${COMPREPLY[@]}" && return 0
+  # main logic
+  local opt filter="cat"
+  while getopts us opt; do
+    case "$opt" in
+      s)  filter="sort";;
+      u)  filter="sort -u"; break;;
+    esac
+  done
+  local IFS0=$IFS IFS=$'\n'
+  COMPREPLY=($(eval "$filter" <<< "${COMPREPLY[*]}" | peco --query "$cur"))
+  IFS=$IFS0
+  # Call recursive, because peco's result may be multiple.
+  "$FUNCNAME"
 }
 
 _zunlib_equals_all_same_index_char() {
