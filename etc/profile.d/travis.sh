0f23564bb05cec81d329a0f378b264d0dfc25ebf
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-02-12 14:04:21 +0900

    Add travis env

---
+++ etc/profile.d/travis.sh
@@ -0,0 +1,17 @@
+# install check
+if ! type -P travis >/dev/null 2>&1; then
+  travis() {
+    if type -P travis >/dev/null 2>&1; then
+      unset -f travis
+      command travis "$@"
+    else
+      echo "travis is not installed. Let's install it." 2>&1
+      echo ">>> sudo gem install travis --no-rdoc --no-ri" 2>&1
+      return 1
+    fi
+  }
+fi
+
+# added by travis gem
+[ -f /Users/kawaz/.travis/travis.sh ] && source /Users/kawaz/.travis/travis.sh
+
