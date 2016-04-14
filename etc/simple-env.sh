91aa1feff00b245ba1b77d32837b1207f2d3fbd4
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-04-14 12:53:43 +0900

    リモートの共用サーバでkawaz-envを読んでる場合、接続元が僕なら自動実行されるようにした

--- etc/simple-env.sh
+++ etc/simple-env.sh
@@ -2,5 +2,13 @@
 kawaz-env() {
   export DOTFILES_DIR=$(cd "$(dirname "$BASH_SOURCE")/.." && pwd)
   . "$DOTFILES_DIR/config/bash/bashrc"
+  if [[ ! $XMODIFIERS =~ @iam= ]]; then
+    export XMODIFIERS="${XMODIFIERS:+$XMODIFIERS }@iam=kawaz"
+  fi
+  kawaz-env() { :; }
   return 0
 }
+
+if grep -qE '(^| )@iam=kawaz( |$)' <<<"$XMODIFIERS"; then
+  kawaz-env
+fi
