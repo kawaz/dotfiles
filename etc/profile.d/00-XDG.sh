a68df34ce5ee624f4b438505ba763b23230e3046
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-04-19 09:14:42 +0900

    Add default XDG Direcoty memo

--- etc/profile.d/00-XDG.sh
+++ etc/profile.d/00-XDG.sh
@@ -2,6 +2,10 @@
 if [[ -n $DOTFILES_DIR ]]; then
   export XDG_CONFIG_HOME=$DOTFILES_DIR/config
   export XDG_CACHE_HOME=$DOTFILES_DIR/cache
-  export XDG_DATA_HOME=$DOTFILES_DIR/local
-  export INPUTRC="$XDG_CONFIG_HOME"/readline/inputrc
+  export XDG_DATA_HOME=$DOTFILES_DIR/local/share
+else
+  export XDG_CONFIG_HOME=~/.config
+  export XDG_CACHE_HOME=~/.cache
+  export XDG_DATA_HOME=~/.local/share
 fi
+export INPUTRC="$XDG_CONFIG_HOME"/readline/inputrc
