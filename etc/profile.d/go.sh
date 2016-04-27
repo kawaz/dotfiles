28e2e30a25e887437f087c62353681a86a235862
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-04-27 15:10:01 +0900

    Add: set GOROOT

--- etc/profile.d/go.sh
+++ etc/profile.d/go.sh
@@ -1,13 +1,13 @@
 # bash
 
-for GOROOT in /usr/local/opt/go/libexec "${XDG_DATA_HOME:-~/.local/share}/go"; do
-  echo $GOROOT
-  if [[ -d $GOROOT && ":$PATH:" != *":$GOROOT/bin:"* ]]; then
-    export PATH=$PATH:$GOROOT/bin
-    break
-  fi
-done
-unset GOROOT
+if [[ -z $GOROOT ]]; then
+  for GOROOT in /usr/local/opt/go/libexec "${XDG_DATA_HOME:-~/.local/share}/go"; do
+    if [[ -d $GOROOT && ":$PATH:" != *":$GOROOT/bin:"* ]]; then
+      export GOROOT PATH=$PATH:$GOROOT/bin
+      break
+    fi
+  done
+fi
 
 if [[ -z $GOPATH ]] && type -p go >/dev/null; then
   export GOPATH="${XDG_DATA_HOME:-~/.local/share}/gopath"
