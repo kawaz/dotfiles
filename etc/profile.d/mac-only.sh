08407dbc570d186567ffba56ec5aed68f5d2eec8
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-05-21 14:51:10 +0900

    Cleanup: shellcheck

--- etc/profile.d/mac-only.sh
+++ etc/profile.d/mac-only.sh
@@ -1,24 +1,29 @@
+#!/bin/bash
+
 # Mac以外では無視
-[[ "${OSTYPE:0:6}" != "darwin" ]] && return
+[[ $OSTYPE == darwin* ]] && return
 
-# coreutils
-export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+# shellcheck disable=SC2155,SC1090
+{
+  # coreutils
+  export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 
-# findutils
-alias find=gfind
-alias xargs=gxargs
+  # findutils
+  alias find=gfind
+  alias xargs=gxargs
 
-# bash_completion
-if [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
-  . $(brew --prefix)/share/bash-completion/bash_completion
-fi
+  # bash_completion
+  if [[ -f $(brew --prefix)/share/bash-completion/bash_completion ]]; then
+    . "$(brew --prefix)/share/bash-completion/bash_completion"
+  fi
 
-# DOCKER_HOSTを設定する
-if type boot2docker >/dev/null 2>&1; then
-  export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375
-fi
+  # DOCKER_HOSTを設定する
+  if type boot2docker >/dev/null 2>&1; then
+    export DOCKER_HOST="tcp://$(boot2docker ip 2>/dev/null):2375"
+  fi
 
-# awscli completion
-if [[ -x /usr/local/bin/aws_completer ]]; then
-  complete -C '/usr/local/bin/aws_completer' aws
-fi
+  # awscli completion
+  if [[ -x /usr/local/bin/aws_completer ]]; then
+    complete -C '/usr/local/bin/aws_completer' aws
+  fi
+}
