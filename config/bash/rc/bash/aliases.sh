4c7cf700564f45c8382fc77d7f0b45398e31d27f
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-06-10 10:58:07 +0900

    Add alias ls

--- config/bash/rc/bash/aliases.sh
+++ config/bash/rc/bash/aliases.sh
@@ -2,7 +2,18 @@ alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
 
-alias ls='ls --color'
+# 可能な限り好みのls出力にする
+if ls -d -N >/dev/null 2>&1; then
+  # 最近の gnubin/ls が勝手にファイル名をクオート出力してくるのを抑止
+  alias ls='ls --color --time-style +%Y-%m-%d\ %H:%M:%S.%3N -N'
+elif ls -d --time-style +%Y-%m-%d\ %H:%M:%S.%3N >/dev/null 2>&1; then
+  # デフォの時間表示は見難いのでISOぽくする(full-iso,long-iso,isoは帯に短し襷に長しなのでカスタム)
+  alias ls='ls --color --time-style +%Y-%m-%d\ %H:%M:%S.%3N'
+elif ls -d --color >/dev/null 2>&1; then
+  # せめて色だけでも…
+  alias ls='ls --color'
+fi
+
 alias grep='grep --color'
 alias egrep='egrep --color'
 
