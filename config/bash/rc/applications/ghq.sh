2101cd3f7eb586156841be43b43961d7f2ec3f08
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-06-12 22:13:28 +0900

    ghq list がシンボリックリンクを見つけてくれないので自分で探すことにした

--- config/bash/rc/applications/ghq.sh
+++ config/bash/rc/applications/ghq.sh
@@ -1,10 +1,15 @@
-ghq-cd() { local d=$(peco --select-1 --layout bottom-up < <(ghq list "$@")); cd "${d:+$HOME/.ghq/$d}"; }
-alias ghcd=ghq-cd
 # ソースファイルは全部ここに集める
 export GHQ_ROOT="${XDG_DATA_HOME:-~/.local/share}/repos"
 
+# peco で移動
+ghq-cd() {
+  local d
+  d=$(cd "$GHQ_ROOT" && for d in */*/*/.git; do echo "${d%/.git}"; done | peco --select-1 --layout bottom-up --query "$READLINE_LINE$*")
+  if [[ -n $d ]]; then
+    cd "$GHQ_ROOT/$d" || return 1
   fi
 }
+alias ghcd='ghq-cd'
 
 # C-s で即移動開始（別途 stty stop undef でデフォルトの C-s の機能を殺しておく必要あり）
 bind -x '"\C-s": "ghq-cd"'
