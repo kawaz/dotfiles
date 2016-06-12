99471990989f95c31091506de80ff98329811b65
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-06-12 22:04:21 +0900

    go や ghq のソースコード置き場が $XDG_DATA_HOME/repos になるようにした

--- config/bash/rc/applications/ghq.sh
+++ config/bash/rc/applications/ghq.sh
@@ -1,5 +1,7 @@
 ghq-cd() { local d=$(peco --select-1 --layout bottom-up < <(ghq list "$@")); cd "${d:+$HOME/.ghq/$d}"; }
 alias ghcd=ghq-cd
+# ソースファイルは全部ここに集める
+export GHQ_ROOT="${XDG_DATA_HOME:-~/.local/share}/repos"
 
 #
 # bash completion support for ghq(https://github.com/motemen/ghq)
--- config/bash/rc/applications/go.sh
+++ config/bash/rc/applications/go.sh
@@ -1,4 +1,4 @@
-# bash
+type -P go >/dev/null || return 0
 
 if [[ -z $GOROOT ]]; then
   for GOROOT in /usr/local/opt/go/libexec "${XDG_DATA_HOME:-~/.local/share}/go"; do
@@ -8,11 +8,13 @@ if [[ -z $GOROOT ]]; then
     fi
   done
 fi
+export GOPATH="${XDG_DATA_HOME:-~/.local/share}/gopath"
 
-if [[ -z $GOPATH ]] && type -p go >/dev/null; then
-  export GOPATH="${XDG_DATA_HOME:-~/.local/share}/gopath"
-fi
+# $GOPATH/src は $XDG_DATA_HOME/repos へのsymlinkにする
+[[ -d $GOPATH ]] || mkdir -p "$GOPATH"
+[[ -h $GOPATH/src ]] || ln -sfn ../repos "$GOPATH/src"
 
+# $GOPATH/bin に PATH を通す
 if [[ -n $GOPATH && ":$PATH:" != *":$GOPATH/bin:"* ]]; then
   export PATH="$PATH:$GOPATH/bin"
 fi
