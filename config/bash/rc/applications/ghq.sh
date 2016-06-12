d7184c369e9d7d26ff05a75354db253f142c6b5c
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-06-12 22:06:18 +0900

    ghqの補完は使わないから削除

--- config/bash/rc/applications/ghq.sh
+++ config/bash/rc/applications/ghq.sh
@@ -3,22 +3,5 @@ alias ghcd=ghq-cd
 # ソースファイルは全部ここに集める
 export GHQ_ROOT="${XDG_DATA_HOME:-~/.local/share}/repos"
 
-#
-# bash completion support for ghq(https://github.com/motemen/ghq)
-#
-_ghq() {
-  local cur prev
-  cur=${COMP_WORDS[COMP_CWORD]}
-  prev=${COMP_WORDS[COMP_CWORD-1]}
-  COMPREPLY=()
-  if (( $COMP_CWORD <= 1)) || [[ "$cur" == -* ]]; then
-    COMPREPLY=( $(compgen -W "$(ghq help | sed -n -e '/COMMANDS:/,/GLOBAL OPTIONS:/p' | sed -n -e 's/   \([a-z]*\).*/\1/p')" -- $cur) );
-    return 0;
-  elif [ $COMP_CWORD = 2 ]; then
-    if [ "$prev" = "look" ]; then
-      COMPREPLY=( $(compgen -W "$(ghq list)" -- $cur) );
-      return 0;
-    fi
   fi
 }
-complete -F _ghq -o default ghq
