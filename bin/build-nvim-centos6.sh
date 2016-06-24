c5af27472444523e5af3d613fa79308262478d38
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-06-24 17:14:56 +0900

    better search pip3, python3

--- bin/build-nvim-centos6.sh
+++ bin/build-nvim-centos6.sh
@@ -3,30 +3,27 @@ set -e
 [[ -z $DOTFILES_LOCAL ]] && { echo "\$DOTFILES_LOCAL is empty" >&2; exit 1; }
 sudo yum -y install libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip git
 
-pip3_path() {
-  local pip3 py3_ver
-  if type -P pip3 >/dev/null 2>&1; then
-    pip3=pip3
-  else
+install_python_neovim() {
+  local pip3 python3
+  pip3=$(compgen -c pip3 || compgen -c pip-3 | sort -rV | head -n 1)
+  if [[ -z $pip3 ]]; then
     py3_ver=$(yum list python3\*-devel -q | grep -Eo '^python3[0-9]+' | sort | tail -n1)
-    [[ -z $py3_ver ]] && return 1
-    sudo yum install -y "${py3_ver}"-devel
-    if yum list "${py3_ver}-pip" >/dev/null 2>&1; then
-      sudo yum install -y "${py3_ver}"-pip
-      pip3=$(rpm -ql "${py3_ver}-pip" | grep -Eo '^([/a-z]*)/bin/pip.*?3.*' | head -n1)
-    else
-      curl https://bootstrap.pypa.io/get-pip.py | sudo "$py3_ver"
-      if type -P pip3 2>/dev/null; then
-        pip3=pip3
-      fi
+    if [[ -z $py3_ver ]]; then
+      return 1
+    fi
+    sudo yum install -y "${py3_ver}"{,-{devel,pip}}
+    python3=$(compgen -c python3 || compgen -c python-3 | sort -rV | head -n 1)
+    pip3=$(compgen -c pip3 || compgen -c pip-3 | sort -rV | head -n 1)
+    if [[ -z $pip3 && -n $python3 ]]; then
+      curl https://bootstrap.pypa.io/get-pip.py | sudo "$python3"
+      pip3=$(compgen -c pip3 || compgen -c pip-3 | sort -rV | head -n 1)
     fi
   fi
   [[ -z $pip3 ]] && return 1
-  type -P "$pip3"
+  sudo "$(type -P "$pip3")" install --upgrade neovim
 }
 
-# build nvim
-{
+install_neovim() {
   tmp=$(mktemp -d "${TMPDIR:-/tmp}/${1:-tmpspace}.XXXXXXXXXX")
   [[ -z $tmp ]] && exit 1
   trap "rm -rf $(printf %q "$tmp")" EXIT
@@ -38,5 +35,5 @@ pip3_path() {
   make install
 }
 
-# pip install neovim
-sudo "$(pip3_path)" install --upgrade neovim
+install_python_neovim
+install_neovim
