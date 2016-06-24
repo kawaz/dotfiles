9c3a8852e2a46fd68c0c8b75ad485fc2b44fd820
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-06-24 17:43:15 +0900

    Add install python3 for nvim

--- bin/build-nvim-centos6.sh
+++ bin/build-nvim-centos6.sh
@@ -7,13 +7,17 @@ install_python_neovim() {
   local pip3 python3
   pip3=$(compgen -c pip3 || compgen -c pip-3 | sort -rV | head -n 1)
   if [[ -z $pip3 ]]; then
-    py3_ver=$(yum list python3\*-devel -q | grep -Eo '^python3[0-9]+' | sort | tail -n1)
-    if [[ -z $py3_ver ]]; then
-      return 1
-    fi
-    sudo yum install -y "${py3_ver}"{,-{devel,pip}}
     python3=$(compgen -c python3 || compgen -c python-3 | sort -rV | head -n 1)
-    pip3=$(compgen -c pip3 || compgen -c pip-3 | sort -rV | head -n 1)
+    if [[ -z $python3 ]]; then
+      py3_ver=$(yum list python3\*-devel -q | grep -Eo '^python3[0-9]+' | sort | tail -n1)
+      if [[ -n $py3_ver ]]; then
+        sudo yum install -y "${py3_ver}"{,-{devel,pip}}
+      else
+        install_python3
+      fi
+      python3=$(compgen -c python3 || compgen -c python-3 | sort -rV | head -n 1)
+      pip3=$(compgen -c pip3 || compgen -c pip-3 | sort -rV | head -n 1)
+    fi
     if [[ -z $pip3 && -n $python3 ]]; then
       curl https://bootstrap.pypa.io/get-pip.py | sudo "$python3"
       pip3=$(compgen -c pip3 || compgen -c pip-3 | sort -rV | head -n 1)
@@ -21,6 +25,19 @@ install_python_neovim() {
   fi
   [[ -z $pip3 ]] && return 1
   sudo "$(type -P "$pip3")" install --upgrade neovim
+}
+
+install_python3() {
+  tmp=$(mktemp -d "${TMPDIR:-/tmp}/${1:-tmpspace}.XXXXXXXXXX")
+  [[ -z $tmp ]] && exit 1
+  trap "rm -rf $(printf %q "$tmp")" EXIT
+  cd "$tmp" || exit 1
+  sudo yum -y groupinstall "Development tools"
+  sudo yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel
+  curl https://www.python.org/ftp/python/3.6.0/Python-3.6.0a1.tgz | tar xz -C . --strip=1
+  ./configure --prefix="$DOTFILES_LOCAL"
+  make
+  make altinstall
 }
 
 install_neovim() {
