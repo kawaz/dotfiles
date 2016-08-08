8870a9f949fe4c2c406f1a804862f356393dff27
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-08-09 08:13:11 +0900

    Add -p option for vim/nvim

--- config/bash/rc/applications/nvim.sh
+++ config/bash/rc/applications/nvim.sh
@@ -1,6 +1,6 @@
-if type -p nvim >/dev/null; then
-  alias vim=nvim
-  alias vim0='command vim -u "$XDG_CONFIG_HOME/vim/vimrc"'
+if type -P nvim >/dev/null; then
+  alias vim='nvim -p'
+  alias vim0='command vim -p -u "$XDG_CONFIG_HOME/vim/vimrc"'
 else
-  alias vim='command vim -u "$XDG_CONFIG_HOME/vim/vimrc"'
+  alias vim='command vim -p -u "$XDG_CONFIG_HOME/vim/vimrc"'
 fi
