ebc39dd64c4f916bdc3e5a4b2f6fe0af83e23ca2
Yoshiaki Kawazu <kawazzz@gmail.com>
2016-09-26 09:57:21 +0900

    Change repos to GOPATH from XDG_DATA_HOME

--- config/bash/rc/applications/ghq.sh
+++ config/bash/rc/applications/ghq.sh
@@ -1,2 +1,3 @@
 # ソースファイルは全部ここに集める
-export GHQ_ROOT="${XDG_DATA_HOME:-~/.local/share}/repos"
+#export GHQ_ROOT="${XDG_DATA_HOME:-~/.local/share}/repos"
+export GHQ_ROOT="${XDG_DATA_HOME:-~/.local/share}/gopath/src"
