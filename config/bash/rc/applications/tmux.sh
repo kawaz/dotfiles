0fd152c6d441933b7f3332b04eefdd445db47abb
Yoshiaki Kawazu <kawazzz@gmail.com>
2019-05-21 15:16:38 +0900

    Add UTF-8 support for tmux

--- config/bash/rc/applications/tmux.sh
+++ config/bash/rc/applications/tmux.sh
@@ -2,12 +2,12 @@
 
 # $XDG_CONFIG_HOME 対応
 _tmux_with_xdg() {
+  local opts=( -u )
   if [[ -n $XDG_CONFIG_HOME ]]; then
     export TMUX_PLUGIN_MANAGER_PATH="$XDG_CACHE_HOME/tpm/plugins"
-    command tmux -f "$XDG_CONFIG_HOME/tmux/tmux.conf" "$@"
-  else
-    command tmux "$@"
+    opts+=( -f "$XDG_CONFIG_HOME/tmux/tmux.conf" )
   fi
+  command tmux "${opts[@]}" "$@"
 }
 
 #tmuxで既存セッションがあればnew-sessionせずにアタッチする
